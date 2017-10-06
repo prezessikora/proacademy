0baf7954f1ee4587d5d1d2e008c51fc80398ac94
Krzysztof Sikora <krzysztof.sikora@ocado.com>
2017-10-06 12:57:34 +0200

    Added artist details view controllers

---
+++ ProAcademy/ArtistViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ArtistViewController.swift
+//  ProAcademy
+//
+//  Created by Krzysztof Sikora on 06/10/2017.
+//  Copyright © 2017 Design Love. All rights reserved.
+//
+
+import UIKit
+
+class ArtistViewController: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var companyTitle: UILabel!
+    
+    @IBOutlet weak var fbButton: UIButton!
+    
+    @IBOutlet weak var info: UITextView!
+    
+    @IBOutlet weak var linkedButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = true
+        
+        configureButton(button: fbButton)
+        configureButton(button: linkedButton)
+
+    }
+    
+    func configureButton(button: UIButton) {
+        button.layer.masksToBounds = false
+        button.clipsToBounds = true
+        button.layer.cornerRadius = fbButton.frame.height/2
+        button.backgroundColor = UIColor.lightGray
+        button.setTitleColor(UIColor.black, for: .normal)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        self.tabBarController?.tabBar.isHidden = false
+    }
+}
