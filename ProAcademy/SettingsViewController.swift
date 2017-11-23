7f01e16b7eb81225dc922c44ef0f987cac26bf00
Krzysztof Sikora <krzysztof.sikora@ocado.com>
2017-11-23 14:37:24 +0100

    Complete settings controller

--- ProAcademy/SettingsViewController.swift
+++ ProAcademy/SettingsViewController.swift
@@ -8,13 +8,30 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    
+    @IBOutlet weak var aboutButton: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         let profile = SessionManager.shared.profile
         profile?.name
+        nameTextField.delegate = self
         
+        aboutButton.setImage(UIImage(named: "right arrow"), for: .normal)
         
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("Begin")
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("End")
     }
     
     @IBAction func onLogout(_ sender: Any) {
