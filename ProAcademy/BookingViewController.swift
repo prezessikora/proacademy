2a90125f2c5765d31593067f0aa5c3a7ee0cd0b7
Krzysztof Sikora <krzysztof.sikora@ocado.com>
2017-12-01 17:01:49 +0100

    Fixed layout issues with booking view

--- ProAcademy/BookingViewController.swift
+++ ProAcademy/BookingViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class BookingViewController: UIViewController {
 
-    @IBOutlet weak var scrollView: UIScrollView!
+    // user
+    
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var userEmail: UITextField!
+    @IBOutlet weak var userPhoneNumber: UITextField!
+    @IBOutlet weak var location: UITextField!
+    
+    // training
+    
+    @IBOutlet weak var trainerName: UILabel!
+    
+    @IBOutlet weak var trainingName: UILabel!
+    
+    @IBOutlet weak var trainingCost: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        userName.isUserInteractionEnabled = false
+        userEmail.isUserInteractionEnabled = false
+        location.isUserInteractionEnabled = false
     }
     
 }
